b55f01621dba9531c613b7179781677b0ee175c1
jrenzoo <u202118568@upc.edu.pe>
2022-07-04 23:57:10 -0500

    Initial commit

---
+++ src/main/java/quest/culture/partner/source/CultureQuestApplication.java
@@ -0,0 +1,13 @@
+package quest.culture.partner.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultureQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/quest/culture/partner/source/CultureQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package quest.culture.partner.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
